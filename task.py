0b99f12030e62632eceafd77d4b610d537e759da
m1st0rm <mishaboy333@gmail.com>
2024-10-05 10:53:27 +0300

    Deleted useless commentary in task.py

--- task.py
+++ task.py
@@ -97,15 +97,6 @@ df_task2 = (
 )
 
 # Task 3. Вывести категорию фильмов, на которую потратили больше всего денег.
-# select category.name, sum(payment.amount) as spent_on_category
-# from category
-# inner join film_category on category.category_id = film_category.category_id
-# inner join inventory on film_category.film_id = inventory.film_id
-# inner join rental on inventory.inventory_id = rental.inventory_id
-# inner join payment on rental.rental_id = payment.rental_id
-# group by category.category_id, category.name
-# order by spent_on_category desc
-# limit 1
 
 df_task3 = (
     df_category.join(
